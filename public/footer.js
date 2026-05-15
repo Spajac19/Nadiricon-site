187759347f524d586b205eedc53fa7e8f2809f88
Toad5x <39568458+Toad5x@users.noreply.github.com>
2026-05-15 17:04:33 -0500

    Commented Saint Peter intercessory prayer
    also, changed the word culmnation to the word portmanteau in the about page for gramatical correctness.

--- public/footer.js
+++ public/footer.js
@@ -1,7 +1,9 @@
 document.getElementById('footer').innerHTML = `
   <footer>
 
-  	<p style="text-align: center; font-size: 5px;">Holy Saint Peter Pray to God for us <img src='images/stpetercross.gif'> Holy Saint Peter Pray to God for us </p>
+  	<!--Holy Saint Peter Pray to God for us-->
+    <p><img src='images/stpetercross.gif'></p>
+    <!--Holy Saint Peter Pray to God for us-->
 	<p>  </p>
     <p> <img src='images/firefox.gif'> <img src='images/drunk.gif'><img src='images/foobar.gif'> <img src='images/fspeech.gif'></p>
     <p><img src='images/anarchy.gif'><img src='images/1080p.gif'><img src='images/db.gif'> </p>
